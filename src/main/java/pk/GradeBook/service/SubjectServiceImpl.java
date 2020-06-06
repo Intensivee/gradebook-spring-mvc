f80fe2306162786fb66f4a0cb25bb10d4f038893
sebastianobora <cawbayt@gmail.com>
2020-06-06 21:55:49 +0200

    Create admin panel: user-subject works

--- src/main/java/pk/GradeBook/service/SubjectServiceImpl.java
+++ src/main/java/pk/GradeBook/service/SubjectServiceImpl.java
@@ -2,10 +2,31 @@ package pk.GradeBook.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pk.GradeBook.model.Subject;
 import pk.GradeBook.repository.SubjectRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SubjectServiceImpl implements SubjectService {
+
     @Autowired
     private SubjectRepository repo;
+
+    @Override
+    public void save(Subject subject) {
+        repo.save(subject);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        repo.deleteById(id);
+    }
+
+    @Override
+    public List<Subject> findAll() {
+        return repo.findAll();
+    }
+
 }
